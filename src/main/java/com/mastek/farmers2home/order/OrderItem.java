b65cb05be9c44d98110e706b0a51274f862a3c98
joe61081 <joe61081@mastek.com>
2020-09-02 14:45:42 +0100

    Add OderItem Class

---
+++ src/main/java/com/mastek/farmers2home/order/OrderItem.java
@@ -0,0 +1,41 @@
+package com.mastek.farmers2home.order;
+
+public class OrderItem {
+
+    int orderId;
+    int productId;
+    int quantity;
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderItem{" +
+                "orderId=" + orderId +
+                ", productId=" + productId +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
