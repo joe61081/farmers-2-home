30c92bdf1b67a97584229f04ccaca5d67fca7778
Hellen61291 <Hellen61291@GB-LD1LPW10G010.mastek.com>
2020-09-03 09:56:05 +0100

    Completed the Entities for Orders and Payments

--- src/main/java/com/mastek/farmers2home/order/Orders.java
+++ src/main/java/com/mastek/farmers2home/order/Orders.java
@@ -1,5 +1,16 @@
 package com.mastek.farmers2home.order;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+@Entity
 public class Orders {
+	int OrderId;
+	double OrderPrice;
+	orderStatus OrderStatus;
+	Date OrderDate;
+	boolean subscription;
+	
 
 }
--- src/main/java/com/mastek/farmers2home/payment/Payments.java
+++ src/main/java/com/mastek/farmers2home/payment/Payments.java
@@ -1,5 +1,15 @@
 package com.mastek.farmers2home.payment;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Payments {
+	int PaymentId;
+	double TotalPaid;
+	PaymentType PaymentType;
+	String PaymentDate;
+	long CardNumber;
+	int SortCode;
+	long AccountNumber;
 
 }
