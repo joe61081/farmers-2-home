8e656c48195431b1ded0fa9b1b5a90c9d49d94c4
HellenNdungu <hellen.w.ndungu@gmail.com>
2020-09-09 17:03:43 +0100

    Hellen #test

---
+++ src/main/java/com/mastek/farmers2home/payment/PaymentJPADAO.java
@@ -0,0 +1,10 @@
+package com.mastek.farmers2home.payment;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentJPADAO extends 
+	CrudRepository<Payment, Integer> {
+
+}
