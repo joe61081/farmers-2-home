563786cf573f441d025c2331022eb3c4c8e90e57
joe61081 <joe61081@mastek.com>
2020-09-10 13:08:24 +0100

    rm redundant annotations

--- src/main/java/com/mastek/farmers2home/customer/CustomerService.java
+++ src/main/java/com/mastek/farmers2home/customer/CustomerService.java
@@ -7,7 +7,6 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @RestController
-@Path("/customer/")
 @RequestMapping("/customer")
 public class CustomerService {
 
@@ -15,10 +14,8 @@ public class CustomerService {
     CustomerJPADAO customerRepo;
 
     @POST
-    @Path("/register")
     @PostMapping(path = "/register", consumes = "application/json", produces = "application/json")
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     public Customer addCustomer(Customer newCustomer) {
         customerRepo.save(newCustomer);
         return newCustomer;
