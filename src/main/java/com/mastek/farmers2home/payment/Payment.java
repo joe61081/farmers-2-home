be65de24a05207ca0bae6d447ed048ea224bfbd6
Hellen61291 <Hellen61291@GB-LD1LPW10G010.mastek.com>
2020-09-15 17:37:54 +0100

    My payment Test

--- src/main/java/com/mastek/farmers2home/payment/Payment.java
+++ src/main/java/com/mastek/farmers2home/payment/Payment.java
@@ -2,18 +2,24 @@ package com.mastek.farmers2home.payment;
 
 import com.mastek.farmers2home.order.Order;
 
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
 
+@XmlRootElement
 @Entity
 @Table(name="payment")
 public class Payment {
--- src/test/java/com/mastek/farmers2home/PaymentTest.java
+++ src/test/java/com/mastek/farmers2home/PaymentTest.java
@@ -2,31 +2,30 @@ package com.mastek.farmers2home;
 
 import static org.junit.Assert.assertNotNull;
 
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
-import com.mastek.farmers2home.order.OrderJPADAO;
-import com.mastek.farmers2home.order.OrderService;
 import com.mastek.farmers2home.payment.Payment;
 import com.mastek.farmers2home.payment.PaymentJPADAO;
-import com.mastek.farmers2home.payment.PaymentService;
+
 import com.mastek.farmers2home.payment.PaymentType;
 
 @RunWith(SpringRunner.class)
+@ContextConfiguration(classes=Payment.class,loader=AnnotationConfigContextLoader.class)//Failed to load application content error
 @SpringBootTest
+
 public class PaymentTest {
 	
 	@Autowired
-	PaymentJPADAO paymentdao;
-	@Autowired
-	PaymentService paymentservice;
-	@Autowired
-	OrderJPADAO orderdao;
-	@Autowired
-	OrderService orderservice;
+	PaymentJPADAO paymentDAO;
+	
 	
 	@Test
 	public void testAddPayment() {
@@ -40,7 +39,10 @@ public class PaymentTest {
 		payment.setSortCode(2345678);
 		payment.setTotalPaid(80.00);
 		
-		payment = paymentdao.save(payment);
+		payment = paymentDAO.save(payment);
+		
+		System.out.println(payment); 
+		
 		assertNotNull("Payment Not Added", payment);
 		
 
