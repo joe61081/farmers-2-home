31fe413d87fe9248baf2c3604c9ed673271a14c1
joe61081 <joe61081@mastek.com>
2020-09-16 10:30:09 +0100

    Login Endpoint

--- src/main/java/com/mastek/farmers2home/customer/CustomerService.java
+++ src/main/java/com/mastek/farmers2home/customer/CustomerService.java
@@ -12,15 +12,23 @@ import javax.ws.rs.core.MediaType;
 public class CustomerService {
 
     @Autowired
-    CustomerJPADAO customerRepo;
+    CustomerJPADAO customerDao;
 
     @POST
     @Path("/register")
     @PostMapping(path = "/register", consumes = "application/json", produces = "application/json")
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public Customer addCustomer(@BeanParam Customer newCustomer) {
-        customerRepo.save(newCustomer);
+        customerDao.save(newCustomer);
         return newCustomer;
+    }
+
+    @GET
+    @Path("/login")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Customer getCustomerLogin(@QueryParam("email") String email, @QueryParam("pass") String password){
+        Customer customer = customerDao.findCustomerLogin(email, password);
+        return customer;
     }
 
 }
