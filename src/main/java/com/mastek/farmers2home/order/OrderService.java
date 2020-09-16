55fe9241b1897d9263cc544c02b86c5523948508
Hellen61291 <Hellen61291@GB-LD1LPW10G010.mastek.com>
2020-09-16 09:50:39 +0100

    OrderService class

--- src/main/java/com/mastek/farmers2home/order/OrderService.java
+++ src/main/java/com/mastek/farmers2home/order/OrderService.java
@@ -3,27 +3,30 @@ package com.mastek.farmers2home.order;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 
 import com.mastek.farmers2home.payment.Payment;
 import com.mastek.farmers2home.payment.PaymentJPADAO;
 
 
+@Component
 public class OrderService {
-	
+
 	@Autowired
 	OrderJPADAO orderDAO;
 	
 	@Autowired
 	PaymentJPADAO paymentDAO;
 	
+
 	@Transactional
-	public Order assignOrderToPayment(int OrderId, int paymentId) {
-		Order order = orderDAO.findById(OrderId).get();
+	public Order assignOrdertoPayment(int orderId, int paymentId) {
+		Order order = orderDAO.findById(orderId).get();
 		Payment payment = paymentDAO.findById(paymentId).get();
 		
 		order.getPaymentAssigned();
 		orderDAO.save(order);
 		return order;
-
 }
 }
