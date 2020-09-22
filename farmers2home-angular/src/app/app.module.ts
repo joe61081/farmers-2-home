4cff83d778a8d90f996a7c2bdd9e069db72c3fac
joe61081 <joe61081@mastek.com>
2020-09-22 12:26:06 +0100

    Customer Register Form

--- farmers2home-angular/src/app/app.module.ts
+++ farmers2home-angular/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customer/customer.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { CustomerComponent } from './customer/customer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- farmers2home-angular/src/app/customer/customer.component.ts
+++ farmers2home-angular/src/app/customer/customer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-customer',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+  customerRegisterForm: FormGroup;
 
-  constructor() { }
+  customerId: number;
+  emailAddress: string; 
+  password: string;
+  customerName: string;
+  customerAddress: string;
+  customerContact: string;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+  //   this.customerRegisterForm = this.formBuilder.group({
+  //     customerName: ['', Validators.required],
+  //     customerEmail: ['', Validators.required],
+  //     customerPassword: ['', Validators.required],
+  //     customerAddress: ['', Validators.required],
+  //     customerContact: ['', Validators.required]
+  // });
   }
+
+  onSubmit() {
+  }
+  
 
 }
