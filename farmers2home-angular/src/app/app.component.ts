682766794fc20f21146afe6af2e3747fc9d83ddb
joe61081 <joe61081@mastek.com>
2020-09-22 18:07:11 +0100

    Added Dark theme toggle

--- farmers2home-angular/src/app/app.component.ts
+++ farmers2home-angular/src/app/app.component.ts
@@ -1,10 +1,16 @@
+import { style } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'farmers2home-angular';
+
+  themeMode = true;
+
+  toggleTheme(){
+      this.themeMode = !this.themeMode;
+  }
 }
