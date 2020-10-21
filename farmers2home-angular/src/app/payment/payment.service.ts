6c64023fbb4bb6b0133ef45dc26aba51e2705521
Michael Bramhall <Michael61296@Mastek.Com>
2020-10-21 14:33:28 +0100

    added payment

--- farmers2home-angular/src/app/payment/payment.service.ts
+++ farmers2home-angular/src/app/payment/payment.service.ts
@@ -9,11 +9,8 @@ import { Payment } from '../payment';
   })
 export class PaymentService {
     rootURL: string;
-    isRegistered:boolean;
-  currentPayment:Payment
 
   constructor(private httpsvc: HttpClient) {
-    this.currentPayment = JSON.parse(localStorage.getItem("currentPayment"));
 
     this.rootURL = "http://localhost:8080/farmers2home"
    }
@@ -22,53 +19,28 @@ export class PaymentService {
       headers: new HttpHeaders({"Content-Type":"application/x-www-form-urlencoded"})
     }
     const params = new URLSearchParams();
-    params.set('PaymentType', newPayment.PaymentType)
-    params.set('CardNumber', newPayment.CardNumber)
-    params.set('SecurityCode', newPayment.SecurityCode)
-    params.set('BillingAddress', newPayment.BillingAddress)
-    params.set('FirstName', newPayment.FirstName)
-    params.set('LastName', newPayment.LastName)
-    params.set('ShippingAddress', newPayment.ShippingAddress)
-    params.set('ShippingMethod', newPayment.ShippingMethod)
-    params.set('Email', newPayment.Email)
-    params.set('Password',newPayment.Password)
+    params.set('firstName', newPayment.firstName)
+    params.set('lastName', newPayment.lastName)
+    params.set('email', newPayment.email)
+    params.set('address', newPayment.address)
+    params.set('city', newPayment.city)
+    params.set('county', newPayment.county)
+    params.set('postCode', newPayment.postCode)
+    params.set('nameOnCard', newPayment.nameOnCard)
+    params.set('CardNumber', newPayment.cardNumber)
+    params.set('expiryMonth', newPayment.expiryMonth)
+    params.set('expiryYear', newPayment.expiryYear)
+    params.set('cvv', newPayment.cvv)
+    params.set('shippingMethod', newPayment.shippingMethod)
+  
 
 
-    return this.httpsvc.post<Farmer>(this.rootURL+"/Payment/register", params.toString(), httpOpts).subscribe(
+    return this.httpsvc.post<Farmer>(this.rootURL+"/payment/register", params.toString(), httpOpts).subscribe(
       (res) => console.log(res),
       (err) => console.log(err)
     );   
    }
-  getPaymentregistered(email: string, password: string){
-    const httpOpts = {
-      headers: new HttpHeaders({"Content-Type":"application/x-www-form-urlencoded"})
-    }
-    const params = new URLSearchParams();
-    params.set('email', email)
-    params.set('password', password) 
-    
-    return this.httpsvc.post(this.rootURL+"/payment/login", params.toString(), httpOpts).subscribe(
-      (result:any) => {
-        if(result){
-        localStorage.setItem('currentPayment', JSON.stringify(result));
-        this.currentPayment = result;
-        this.isRegistered= true;    
-        }
-      }
-    )
-}
-paymentLogout(){
-  localStorage.removeItem("currentPayment");
-  this.isRegistered = false;
-}
-
-checkIsregistered(){
-  if(localStorage.getItem("currentPayment")){
-    return true;
-  }else{
-    return false;
-  }
-}
+  
 }
 
 
